13fc87084793a0d0c7d7253b62de3e47c74f1363
tone <381347973@qq.com>
2018-02-03 22:41:30 +0800

    dubbo

---
+++ dubbo-customer/src/main/java/com/tone/github/dubbo/customer/Controller.java
@@ -0,0 +1,20 @@
+package com.tone.github.dubbo.customer;
+
+import com.alibaba.dubbo.config.annotation.Reference;
+import com.tone.github.dubbo.api.DemoService;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller {
+    @Reference(version = "1.0.0",
+            application = "${dubbo.application.id}",
+            url = "dubbo://localhost:12345")
+    private DemoService demoService;
+
+    @RequestMapping("/sayHello")
+    public String sayHello(@RequestParam String name) {
+        return demoService.sayHello(name);
+    }
+}
