8c7f5629c35637dad92ccbda7267b398c44b512b
逛逛0277 <381347973@qq.com>
2018-07-06 15:36:43 +0800

    state machine

---
+++ spring-boot-statemachine/src/main/java/com/tone/sm/Events.java
@@ -0,0 +1,12 @@
+package com.tone.sm;
+
+/**
+ * @author zhaoxiang.liu
+ * @date 2018/7/6
+ */
+public enum Events {
+    // 支付
+    PAY,
+    // 收货
+    RECEIVE
+}
---
+++ spring-boot-statemachine/src/main/java/com/tone/sm/States.java
@@ -0,0 +1,14 @@
+package com.tone.sm;
+
+/**
+ * @author zhaoxiang.liu
+ * @date 2018/7/6
+ */
+public enum States {
+    // 待支付
+    UNPAID,
+    // 待收货
+    WAITING_FOR_RECEIVE,
+    // 结束
+    DONE
+}
