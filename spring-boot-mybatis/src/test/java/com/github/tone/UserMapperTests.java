f06c25bd93006491c6d0b0e50dd38f7285e03beb
逛逛0277 <381347973@qq.com>
2019-02-15 14:50:25 +0800

    加密工具jasypt

--- spring-boot-mybatis/src/test/java/com/github/tone/UserMapperTests.java
+++ spring-boot-mybatis/src/test/java/com/github/tone/UserMapperTests.java
@@ -1,13 +1,13 @@
 package com.github.tone;
 
 import com.github.tone.dao.UserMapper;
-import com.github.tone.domain.User;
-import org.junit.Assert;
+import com.github.tone.service.TargetService;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.transaction.annotation.Transactional;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -15,13 +15,26 @@ public class UserMapperTests {
 
     @Autowired
     private UserMapper userMapper;
+    @Autowired
+    TargetService targetService;
 
     @Test
-//    @Transactional
+    public void ts() {
+        targetService.a();
+    }
+
+    @Test
+    @Transactional
     public void findByName() throws Exception {
-        userMapper.insert("AAA", 20);
-        User u = userMapper.findByName("AAA");
-        Assert.assertEquals(20, u.getAge().intValue());
+//        userMapper.insert("AAA", 20);
+
+        System.out.println("userMapper读取数据: " + userMapper.findByName("AAA"));
+        System.out.println("userMapper读取数据: " + userMapper.findByName("AAA"));
+        System.out.println(
+                "userMapper更新了" + userMapper.update("AAA", 32) + "个学生的数据");
+        System.out.println("userMapper读取数据: " + userMapper.findByName("AAA"));
+        System.out.println("userMapper读取数据: " + userMapper.findByName("AAA"));
+
     }
 
 }
